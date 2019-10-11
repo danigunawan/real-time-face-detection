0b0475c4e169d4603407bd64e4763f44d0b46c4c
eneso <e.furkanoguz@gmail.com>
2019-10-11 16:47:27 -0600

    Add models folder, face-api.js and  load models

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 //gets the video element created at index.html
 const video = document.getElementById("video")
 
+//loading models which is done asycnrounously (calling them in paralel thus faster to execute)
+//passing in array of promises
+//it takes .then
+Promise.all([
+    //face detector that runs real time
+    faceapi.nets.tnyFaceDetector.loadFromUri('/models'),
+    //registers difference part of the face.
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    //Allow API to recognize where my face is and the box around it
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    //Recognize Expressions (Happy, sad, smile)
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
+
 //hooks up webcam to video element
 function startVideo(){
     //gets the webcam, takes object as the first param, video is key, empty object as param
@@ -11,4 +25,9 @@ function startVideo(){
         err => console.log(err))
 }
 
-startVideo();
+
+//add event listener for when the video starts playing
+//when it starts playing, start recognizing the face
+video.addEventListener("play", ()=>{
+    console.log("testtt")
+})
